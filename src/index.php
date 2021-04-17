78989029414a4488edaba6f1f307152a1adbeed0
spfpaul <spfpaul@hotmail.com>
2021-04-18 01:02:03 +0200

    update index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,14 @@
-<center><h1>
-<?php 
-echo "Hello My First PHP app v3!";
-?>
-</h1></center>
+<html>
+  <body>
+    <center><h1>
+    <?php 
+      echo "Welcome to my demo v3!";
+    ?>
+    </h1></center>
+    <h2>Request Information</h2>
+    <p/>
+    <?php
+      foreach (getallheaders() as $name => $value) { echo "<li>Headers['".$name."']: $value";}
+    ?>
+  </body>
+</html>
